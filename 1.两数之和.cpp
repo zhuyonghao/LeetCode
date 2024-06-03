01922017d7b3643824af6600c7d61715b3b8dbca
zhuyonghao <3113267308@qq.com>
2024-06-03 21:18:16 +0800

    2024/06/03

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+/*
 class Solution
 {
 public:
@@ -19,4 +20,36 @@ public:
         return {};
     }
 };
+*/
+
+// 哈希表
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        // 用来存放已经遍历过的元素
+        unordered_map<int, int> Map;
+
+        // 遍历元素
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // 要查找的元素
+            int s = target - nums[i];
+            // 查找是否存在该元素
+            // find返回一个迭代器
+            auto itr = Map.find(s);
+            if (itr != Map.end())
+            {
+                // 如果查询到返回下标
+                return {itr->second, i};
+            }
+            // 如果不存在将已查询的元素放入map中
+            Map.insert(make_pair(nums[i], i));
+        }
+        // 未查询到返回空
+        return {};
+    }
+};
+
 // @lc code=end
